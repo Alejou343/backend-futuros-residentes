a1f7e34ebce362782d5bccac47c2ae290a39c165
Alejou343 <alejandro.auribe1@gmail.com>
2023-08-14 15:11:54 -0500

    Quiz de matemáticas añadido

---
+++ prueba-futuros-residentes/Databases/questionsMath.js
@@ -0,0 +1,94 @@
+const questionsMath = [
+    {
+        "pregunta": "¿Cuál es el resultado de 2 + 2?",
+        "opciones": [
+            {"text": "3", "isCorrect": false},
+            {"text": "4", "isCorrect": true},
+            {"text": "5", "isCorrect": false},
+            {"text": "6", "isCorrect": false}
+        ]
+    },
+    {
+        "pregunta": "Si x = 3 y y = 5, ¿cuál es el valor de x * y?",
+        "opciones": [
+            {"text": "8", "isCorrect": false},
+            {"text": "10", "isCorrect": true},
+            {"text": "15", "isCorrect": false},
+            {"text": "20", "isCorrect": false}
+        ]
+    },
+    {
+        "pregunta": "¿Cuál es el resultado de 12 / 3?",
+        "opciones": [
+            {"text": "2", "isCorrect": false},
+            {"text": "3", "isCorrect": true},
+            {"text": "4", "isCorrect": false},
+            {"text": "6", "isCorrect": false}
+        ]
+    },
+    {
+        "pregunta": "¿Cuál es la raíz cuadrada de 49?",
+        "opciones": [
+            {"text": "6", "isCorrect": false},
+            {"text": "7", "isCorrect": true},
+            {"text": "8", "isCorrect": false},
+            {"text": "9", "isCorrect": false}
+        ]
+    },
+    {
+        "pregunta": "Si a = 4 y b = 7, ¿cuál es el valor de a^2 + b^2?",
+        "opciones": [
+            {"text": "41", "isCorrect": false},
+            {"text": "65", "isCorrect": false},
+            {"text": "65", "isCorrect": true},
+            {"text": "81", "isCorrect": false}
+        ]
+    },
+    {
+        "pregunta": "¿Cuál es el resultado de 3 * (4 + 5)?",
+        "opciones": [
+            {"text": "12", "isCorrect": false},
+            {"text": "27", "isCorrect": true},
+            {"text": "45", "isCorrect": false},
+            {"text": "18", "isCorrect": false}
+        ]
+    },
+    {
+        "pregunta": "Si x = 8 y y = 2, ¿cuál es el valor de x / y?",
+        "opciones": [
+            {"text": "4", "isCorrect": false},
+            {"text": "2", "isCorrect": true},
+            {"text": "6", "isCorrect": false},
+            {"text": "8", "isCorrect": false}
+        ]
+    },
+    {
+        "pregunta": "¿Cuál es el resultado de 15 - 9?",
+        "opciones": [
+            {"text": "6", "isCorrect": true},
+            {"text": "9", "isCorrect": false},
+            {"text": "15", "isCorrect": false},
+            {"text": "24", "isCorrect": false}
+        ]
+    },
+    {
+        "pregunta": "Si a = 5 y b = 3, ¿cuál es el valor de a * b?",
+        "opciones": [
+            {"text": "8", "isCorrect": false},
+            {"text": "15", "isCorrect": false},
+            {"text": "20", "isCorrect": false},
+            {"text": "15", "isCorrect": true}
+        ]
+    },
+    {
+        "pregunta": "¿Cuál es el resultado de 25 / 5?",
+        "opciones": [
+            {"text": "4", "isCorrect": false},
+            {"text": "5", "isCorrect": true},
+            {"text": "6", "isCorrect": false},
+            {"text": "7", "isCorrect": false}
+        ]
+    }
+];
+
+export default questionsMath
