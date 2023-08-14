738b8ec3503616e92c932d98cd82914dcf694006
Alejou343 <alejandro.auribe1@gmail.com>
2023-08-14 13:48:18 -0500

    Actualización de cors

--- prueba-futuros-residentes/pages/api/hello.js
+++ prueba-futuros-residentes/pages/api/hello.js
@@ -1,5 +1,15 @@
 import questions from "@/Databases/questions"
+import cors from 'cors';
+
+const corsOptions = {
+  origin: 'http://127.0.0.1:5500', // Cambia esto por la URL correcta de tu aplicación en localhost
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
 export default function handler(req, res) {
-  res.status(200).json(questions)
+  cors(corsOptions)(req, res, () => {
+    res.status(200).json(questions);
+  });
 }
